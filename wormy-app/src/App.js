fc62bbc3429aa06274d4fb2e0f6ba4f62093fb28
Ashley Madera <ashleymadera@Ashleys-MacBook-Pro.local>
2020-04-27 18:46:52 -0400

    api

--- wormy-app/src/App.js
+++ wormy-app/src/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import { useEffect } from 'react'
+import axios from 'axios'
+
 
 function App() {
+
+
+  useEffect(() => {
+    const apiCall = async () => {
+      const response = await axios('https://www.goodreads.com/search.xml?key=ybeFDV188bV1sTPf7xemw&q=Ender%27s+Game')
+      // would be
+      console.log(response)
+      // updateParks(listOfParks)
+    }
+    apiCall()
+  }, [])
+
+
+
+
   return (
     <div>
       <h1>hello</h1>
