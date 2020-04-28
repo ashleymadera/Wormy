9054fff9172a85424bf96c097aa49756811025ef
Ashley Madera <ashleymadera@Ashleys-MacBook-Pro.local>
2020-04-28 11:34:32 -0400

    commit

--- wormy-app/src/App.js
+++ wormy-app/src/App.js
@@ -1,26 +1,35 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios'
 import { Route } from 'react-router-dom'
 import convert from 'xml-js'
 import BestSeller from './components/BestSeller';
 
+
+
 function App() {
 
 
-  const baseURL =
 
-    // api call for book seeach
-    useEffect(() => {
-      const apiCall = async () => {
-        const response = await axios('https://corsanywhere.herokuapp.com/https://www.goodreads.com/search.xml?key=ybeFDV188bV1sTPf7xemw&q=me+before+you')
+  // api call for book seeach
+  useEffect(() => {
+    const apiCall = async () => {
+      const response = await axios(`https://corsanywhere.herokuapp.com/https://www.goodreads.com/search.xml?key=ybeFDV188bV1sTPf7xemw&q=${input}`)
 
-        console.log(response)
-        const resJSON = convert.xml2json(response.data, { compact: true, spaces: 4 })
-        console.log(JSON.parse(resJSON))
-      }
-      apiCall()
-    }, [])
+      console.log(response)
+      const resJSON = convert.xml2json(response.data, { compact: true, spaces: 4 })
+      console.log(JSON.parse(resJSON))
+    }
 
+  }, [])
+
+
+  const [input, setInput] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Submitting Name ${input}`)
+    apiCall()
+  }
 
 
   // const apiCallBest = async () => {
@@ -33,16 +42,29 @@ function App() {
 
 
 
-
-
   return (
     <>
       <header>
-
         {/* <img src="https://imgur.com/8cIyLoo" alt="Wormy Logo" /> */}
         <h1>Wormy</h1>
-
       </header>
+
+      <div>
+
+      </div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input
+            type="text"
+            placeholder="Search"
+            value={input}
+            onChange={e => setInput(e.target.value)}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+
+      <div></div>
 
       <div>
         <h1>Daily Read</h1>
