a743f1294a7ec3fbd78f543ca4f846d9f0e53ed1
Ashley Madera <ashleymaderamachado@gmail.com>
2022-03-09 23:03:07 -0500

    Frontend reading endpoint

---
+++ wormy-app/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Hello
+
+    <?php
+        // print to screen
+        echo "Hello World";
+
+        $myvar = "this is an variable"
+        $number = 5;
+        $number2 = 3
+        $sum = $number + $number2
+
+
+        //concat
+
+        $name = "jake"
+        echo "Hello " . $name
+
+        //operators: + = / * = $var 
+        //logic operators:  == !== and or
+
+
+        //if
+
+        $loggedIn = true;
+
+        if ($loggedIn == true){
+            echo "Your are logged in!";
+        } else {
+            echo "Please log in!";
+        }
+
+
+
+
+    ?>
+
+
+<form>
+<input type="text">    
+<input type="submit">
+<form/>
+
+
+
+</body>
+</html>
